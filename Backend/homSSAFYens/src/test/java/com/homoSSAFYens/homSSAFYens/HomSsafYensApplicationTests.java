87707e7dfa84c25ddaab613a8a56274b8d164fdb
dh0728 <dustkscjswo@naver.com>
2025-08-12 18:00:13 +0900

    init: dev 브랜치 초기세팅

---
+++ Backend/homSSAFYens/src/test/java/com/homoSSAFYens/homSSAFYens/HomSsafYensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homoSSAFYens.homSSAFYens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomSsafYensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
